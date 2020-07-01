affb3b84ca571e971f384a48847b0c9e1f0f7440
Binjita Kandel Baral <binjita.baral@gmail.com>
2020-07-01 11:53:04 -0500

    array

--- js/iterating.js
+++ js/iterating.js
@@ -6,36 +6,43 @@
      * Create an array of 4 people's names and store it in a variable called
      * 'names'.
      */
-    var names = ["Binju", "Sanju", "Bijit", "Santosh"]
+    var names = ['Binju', "Sanju", "Bijit", "Santosh"];
 
     /**
      * TODO:
      * Create a log statement that will log the number of elements in the names
      * array.
      */
-    console.log(names.length);
+    console.log("There number of element in the names array is " + names.length);
 
     /**
      * TODO:
      * Create log statements that will print each of the names individually by
      * accessing each element's index.
      */
-    names.forEach(function (names, index) {
-    console.log("the names on index : " + index + " is " + names[i]);
-})
+
+    console.log("The names on index is " + names[0]);
+    console.log("The names on index is " + names[1]);
+    console.log("The names on index is " + names[2]);
+    console.log("The names on index is " + names[3]);
+
     /**
      * TODO:
      * Write some code that uses a for loop to log every item in the names
      * array.
      */
-    names.forEach(function (isArray) {
-        console.log("the names is " + names[i]);
-    })
+    for (let i=0; i<names.length;i++){
+        console.log("The name at index" + i + " is " + names[i]);
+    }
+
     /**
      * TODO:
      * Refactor your above code to use a `forEach` loop
      */
-
+    console.log("The name in the array is ");
+    names.forEach(function (name, index,) {
+        console.log("\t" + name);
+    })
     /**
      * TODO:
      * Create the following three functions, each will accept an array and
@@ -49,5 +56,24 @@
      *  > second([1, 2, 3, 4, 5]) // returns 2
      *  > last([1, 2, 3, 4, 5]) // return 5
      */
+
+        //1.
+    let numbers =[1,2,3,4,5];
+    function first (x){
+        return (numbers [0]);
+    }
+    console.log(first (numbers));
+
+    //2.
+    function second (x){
+        return (numbers [1]);
+    }
+    console.log(second (numbers));
+
+    //3.
+    function last (x){
+        return (numbers [numbers.length -1]);
+    }
+    console.log(last (numbers));
 
 })();

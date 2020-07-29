aacc8f4208444da010c905488bce4ad25f5cb5b3
Binjita Kandel Baral <binjita.baral@gmail.com>
2020-07-28 22:26:26 -0500

    exercises

--- js/weathermap.js
+++ js/weathermap.js
@@ -50,64 +50,84 @@ $(document).ready(function () {
             //Assign the pressure
             var pressure = data.daily[i].pressure;
             $("#pressure" + i).html("Pressure : " + "<strong>" + pressure + "</strong>");
-
         }
-        // ------mapbox ---------
-
-        //Creating main map for San Antonio, Texas
-        mapboxgl.accessToken = mapboxKey;
-        var map = new mapboxgl.Map({
-            container: 'map',
-            style: 'mapbox://styles/mapbox/streets-v9',
-            zoom: 10,
-            center: [-98.4916, 29.4252]
-        });
-        // disable map zoom when using scroll
-        map.scrollZoom.disable();
-
-//    creating marker object
-        var marker = new mapboxgl.Marker({
-        })
-            .setLngLat([-98.4916, 29.4252])
-            .addTo(map)
-            .setDraggable(true);
-
-
-        // writing function for marker
-        function onDragEnd() {
-            var lngLat = marker.getLngLat();
-           map.flyTo({center:lngLat});
-            $.get("http://api.openweathermap.org/data/2.5/onecall", {
-                APPID: OPEN_WEATHERMAP_APPID,
-                lat: lngLat.lat,
-                lon: lngLat.lng,
-                units: "imperial"
-            })
-                .done(function (data) {
-                    updateMap(data);
-                });
-        }
-
-        marker.on('dragend', onDragEnd);
     }
 
+    // ------mapbox ---------
 
-//------geocode js
-//         function geocode(search, token) {
-//             var baseUrl = 'https://api.mapbox.com';
-//             var endPoint = '/geocoding/v5/mapbox.places/';
-//             return fetch(baseUrl + endPoint + encodeURIComponent(search) + '.json' + "?" + 'access_token=' + token)
-//                 .then(function (res) {
-//                     return res.json();
-//                     // to get all the data from the request, comment out the following three lines...
-//                 }).then(function (data) {
-//                     return data.features[0].center;
-//                 });
-//         }
+    //Creating main map for San Antonio, Texas
+    mapboxgl.accessToken = mapboxKey;
+    var map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v9',
+        zoom: 10,
+        center: [-98.4916, 29.4252]
+    });
+    // disable map zoom when using scroll
+    map.scrollZoom.disable();
+    map.addControl(new mapboxgl.NavigationControl());
+//    creating marker object
+    var marker = new mapboxgl.Marker({})
+        .setLngLat([-98.4916, 29.4252])
+        .addTo(map)
+        .setDraggable(true);
 
 
-    //---Editing with Jquery-------
+    //---- writing function for marker
+    function onDragEnd() {
+        var lngLat = marker.getLngLat();
+        $.get("http://api.openweathermap.org/data/2.5/onecall", {
+            APPID: OPEN_WEATHERMAP_APPID,
+            lat: lngLat.lat,
+            lon: lngLat.lng,
+            units: "imperial"
+        })
+            .done(function (data) {
+                updateMap(data);
+                map.flyTo({center: [lngLat.lng, lngLat.lat]})
+                map.set
+            });
+    }
 
-    // making heading and p blue color
-    //$("h1,p").css("backgroundColor", "skyblue");
+    marker.on('dragend', onDragEnd);
+
+//-----using geocode for userinput
+
+    function geocode(search, token) {
+
+        var baseUrl = 'https://api.mapbox.com';
+        var endPoint = '/geocoding/v5/mapbox.places/';
+        return fetch(baseUrl + endPoint + encodeURIComponent(search) + '.json' + "?" + 'access_token=' + token)
+            .then(function (res) {
+                return res.json();
+                // to get all the data from the request, comment out the following three lines...
+            }).then(function (data) {
+                console.log(data);
+                return data.features[0].center;
+            });
+    }
+
+    $("#find").click(function () {
+        var userinput = $("#formuserinput").val();
+
+        geocode(userinput, mapboxKey).then(function(data) {
+            alert(userinput);
+        })
+
+    });
+
+//adding places name to current city
+    $("#find").click(function () {
+        var userinput = $("#forminput").val();
+        $(".city").append( userinput);
+    });
+//_____Jquery Effects
+    $('.nav-link').hover(
+        function() {
+            $(this).css('background-color', 'skyblue');
+        },
+        function() {
+            $(this).css('background-color', '#FFF');
+        }
+    );
 });

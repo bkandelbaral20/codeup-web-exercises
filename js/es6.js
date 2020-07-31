6a4be18b2a3b6c7ed39209435880bc1b3776e0bb
Binjita Kandel Baral <binjita.baral@gmail.com>
2020-07-31 09:23:28 -0500

    es6 exercises

--- js/es6.js
+++ js/es6.js
@@ -27,11 +27,6 @@ const users = [
     email: 'justin@codeup.com',
     languages: ['html', 'css', 'javascript', 'php']
   }
-  // {
-  //    name: 'Binjita Kandel',
-  //    email: 'binjita@gmail.com',
-  //    languages: ['html',"css","javascript","java", "jQuery" ]
-  // }
 ];
 
 // TODO: fill in your name and email and add some programming languages you know
@@ -43,28 +38,22 @@ const email = 'binjita@codeup.com';
 const languages = ["html","css","javascript","java","jQuery"];
 
 // TODO: rewrite the object literal using object property shorthand
-users.push({
-   name,
-   email,
-   languages
-});
-console.log(users);
+users.push({name, email, languages });
+//console.log(users);
+
 // TODO: replace `var` with `let` in the following variable declarations
 let emails = [];
 let names = [];
 
 // TODO: rewrite the following using arrow functions
-users.forEach((user) => {
-   emails.push(user.email);
+users.forEach(user => emails.push(user.email));
   // console.log(user.email);
-});
-users.forEach((user) => {
-   names.push(user.name);
+users.forEach(user=> names.push(user.name));
   // console.log(user.name);
-});
 
 // TODO: replace `var` with `let` in the following declaration
 let developers = [];
+
 users.forEach((user) => {
   // TODO: rewrite the code below to use object destructuring assignment
   //       note that you can also use destructuring assignment in the function
@@ -73,7 +62,7 @@ users.forEach((user) => {
  // console.log(user)
 
   // TODO: rewrite the assignment below to use template strings
-  developers.push(`${name}\'s email is  ${email}  ${name} knows ${languages}.`);
+  developers.push(`${name}'s email is  ${email}.  ${name} knows ${languages.join(",")}`);
 });
 
 // TODO: Use `let` for the following variable
@@ -81,10 +70,9 @@ let  list = '<ul>';
 
 // TODO: rewrite the following loop to use a for..of loop
 //developers.forEach(function (developer) {
-for(var developer of developers){
-  console.log(developer);
+for(let developer of developers){
+ // console.log(developer);
 }
   // TODO: rewrite the assignment below to use template strings
-   list += (` '<li>' + ${developer} + '</li>';
-}`);
+   list += `<li>${developer}</li>}`;
 list += '</ul>';

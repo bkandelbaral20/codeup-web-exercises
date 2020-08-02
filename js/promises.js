54bed2cd06dd9daeb61351469fb25dc0a2540c7a
Binjita Kandel Baral <binjita.baral@gmail.com>
2020-08-02 11:51:33 -0500

    exercises

--- js/promises.js
+++ js/promises.js
@@ -1,13 +1,14 @@
-
 //Create a function that accepts a GitHub username, and returns a promise that resolves returning
 // just the date of the last commit that user made. Reference the github api documentation to achieve this.
 
 const api_url = "https://api.github.com/users/bkandelbaral20/events"
-async function getInformation (){
-    const response = await fetch(api_url,{headers: {'Authorization': 'promiseKey'}});
+
+async function getInformation() {
+    const response = await fetch(api_url, {headers: {'Authorization': 'promiseKey'}});
     const data = await response.json();
     console.log(`The latest date of my github commit is: ${data[0].created_at}`);
 }
+
 getInformation();
 
 //Write a function named wait that accepts a number as a parameter, and returns a promise that
